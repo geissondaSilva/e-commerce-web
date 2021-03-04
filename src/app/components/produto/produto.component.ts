c8c5a06659e51458575b51d7cf2d558b2c1cb43c
geissondaSilva <geisonmsilva12@gmail.com>
2021-03-04 17:03:48 -0300

    first commit

---
+++ src/app/components/produto/produto.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-produto',
+    templateUrl: 'produto.component.html'
+})
+
+export class ProdutoComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
